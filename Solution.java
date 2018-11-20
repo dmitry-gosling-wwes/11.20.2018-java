90bb491fd7ec99b571f58ab4f67d005b89884f82
Red-Wagonwheel <red.wagonwheel@gmail.com>
2018-11-20 13:57:41 -0800

    commit to RED branch

--- Solution.java
+++ Solution.java
@@ -4,6 +4,7 @@ public class Solution {
 	public static void main(String[] args) {
 		System.out.println("Hi, team!");
 		System.out.println("Hi, Boss! Red is here!");
+		System.out.println("Creating red branch");
 	}
 
 }
